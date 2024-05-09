ad58acff80d18208eb7d714ecf29cc3869f09075
Nafiad <111278128+NafiadAbdi@users.noreply.github.com>
2024-05-09 03:58:18 -0500

    added netflix favicon and Title

--- netflix-clone/src/Components/Header/Header.jsx
+++ netflix-clone/src/Components/Header/Header.jsx
@@ -3,8 +3,8 @@ import "./header.css";
 import NetflixLogo from "../../assets/images/NetflixLogo.png";
 import SearchIcon from "@mui/icons-material/Search";
 import NotificationsNoneIcon from "@mui/icons-material/NotificationsNone";
-import AccountBoxIcon from '@mui/icons-material/AccountBox';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 const Header = () => {
 	return (
 		<div className="header_outer_container">
@@ -25,10 +25,18 @@ const Header = () => {
 				</div>
 				<div className="header_right">
 					<ul>
-						<li><SearchIcon /></li>
-						<li><NotificationsNoneIcon /></li>
-						<li><AccountBoxIcon/></li>
-						<li><ArrowDropDownIcon/></li>
+						<li>
+							<SearchIcon />
+						</li>
+						<li>
+							<NotificationsNoneIcon />
+						</li>
+						<li>
+							<AccountBoxIcon />
+						</li>
+						<li>
+							<ArrowDropDownIcon />
+						</li>
 					</ul>
 				</div>
 			</div>
